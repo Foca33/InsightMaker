78931343a973375d8365851f5471d05bd6f86693
Andres Rodríguez Escobar <foca@Andress-MacBook-Pro.local>
2025-04-25 21:47:21 -0500

    InsightMaker convertido en PWA 🚀

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,10 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker registrado', reg))
+      .catch(err => console.error('Error registrando Service Worker', err));
+  });
+}
