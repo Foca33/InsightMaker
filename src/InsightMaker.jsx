9378a0235ac66e41ce855f204bc5c14bdeed2f01
Andres Rodríguez Escobar <foca@Andress-MacBook-Pro.local>
2025-04-26 22:01:51 -0500

    InsightMaker Premium v2: contadores, diseño, almacenamiento en Firebase, Dashboard 🎯

--- src/InsightMaker.jsx
+++ src/InsightMaker.jsx
@@ -1,5 +1,5 @@
 import { useState, Fragment } from "react";
-import { db } from "../firebase";
+import { db } from "./firebase";
 import { addDoc, collection } from "firebase/firestore";
 import axios from "axios";
 
